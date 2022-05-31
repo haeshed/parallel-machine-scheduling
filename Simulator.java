2d85e946e0d96d33fc7c503b08d52ad1a10cb955
haeshed <ha.eshed@gmail.com>
2022-05-31 10:20:49 +0300

    order of simulator func

--- Simulator.java
+++ Simulator.java
@@ -35,88 +35,9 @@ public class Simulator {
         Simulator sim1 = new Simulator(5, 0);
         sim1.buildSimulator(jobNum);
 //        sim1.buildSimulator();
-//        System.out.println(sim1.toString());
-//        sim1.testMove(sim1.machines[0].getJob(0), sim1.machines[1]);
         System.out.println(sim1.toString());
-//        System.out.println("decided: " + sim1.bestResponseJob(sim1.machines[0].getJob(1)));
         sim1.runRound();
         System.out.println(sim1.toString());
-    }
-
-    public void testMove(Job job, Machine destMachineID) {
-        try {
-            this.move(job, destMachineID);
-        } catch (Exception e) {
-            System.out.println("Exception: No such job/machine exists.\n");
-        }
-    }
-
-    public Machine bestResponseJob(Job job) {
-        System.out.println("Started BRJ for job: " + job.getID());
-        Machine fromMachine = job.runningMachine;
-        int startIndex = fromMachine.jobList.indexOf(job);
-        Machine toMachine = job.runningMachine;
-        int bestCompTime = job.completionTime;
-        for (Machine machine : this.machines) {
-            this.move(job, machine);
-//            System.out.println(bestCompTime + " <best | cur> " + job.completionTime);
-//            System.out.println(toMachine.ID + " <bestM | curM> " + job.runningMachine.ID);
-//            System.out.println(job.runningMachine.toString());
-            if (job.completionTime < bestCompTime) {
-                System.out.println("-----switched best to mach " + job.runningMachine.ID + " time " + job.completionTime);
-//                System.out.println(job.completionTime + " <time | mach> " + job.runningMachine.ID);
-                bestCompTime = job.completionTime;
-                toMachine = job.runningMachine;
-            }
-        }
-//        System.out.println(this.toString());
-        job.runningMachine.remove(job);
-        fromMachine.jobList.add(startIndex, job);
-        job.setRunningMachine(fromMachine);
-        this.setCompletionTime();
-        return toMachine;
-    }
-
-    private void runRound() {
-        ListIterator iterator = this.allJobs.iterator();
-        while (iterator.current != null) {
-            Machine curMachine = iterator.current.job.runningMachine;
-            Machine bestMachine = this.bestResponseJob(iterator.current.job);
-            if (curMachine != bestMachine) {
-                this.move(iterator.current.job, bestMachine);
-                System.out.println("finished BRJ, moved to: " + iterator.current.job.runningMachine.ID + " <mach | time> " + iterator.current.job.completionTime);
-            } else
-                System.out.println("finished BRJ, stayed at: " + iterator.current.job.runningMachine.ID + " <mach | time> " + iterator.current.job.completionTime);
-            System.out.println();
-            System.out.println(this.toString());
-            iterator.next();
-        }
-    }
-
-    public void runSimulator(int times) {
-        for (int i = 0; i < times; i++) {
-            System.out.println();
-        }
-    }
-
-    public void move(Job job, Machine destMachineID) {
-        int currMachineID = job.runningMachine.getID();
-        machines[currMachineID].remove(job);
-        ListIterator iterator = machines[currMachineID].jobList.iterator();
-        while (iterator.current != null) {
-            iterator.current.job.setCompletionTime();
-            iterator.next();
-        }
-        destMachineID.insert(job);
-//        System.out.println("moved job id: " + job.getID() + "   machines: " + currMachineID + " ---> " + job.runningMachine.ID);
-    }
-
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        for (Machine machine : machines) {
-            s.append(machine.toString());
-        }
-        return s.toString();
     }
 
     public void buildSimulator(int numJobs) {
@@ -137,6 +58,64 @@ public class Simulator {
         }
     }
 
+    public void runSimulator(int times) {
+        for (int i = 0; i < times; i++) {
+            runRound();
+            System.out.println(this.toString());
+        }
+    }
+
+    private void runRound() {
+        ListIterator iterator = this.allJobs.iterator();
+        while (iterator.current != null) {
+            Machine curMachine = iterator.current.job.runningMachine;
+            Machine bestMachine = this.bestResponseJob(iterator.current.job);
+            if (curMachine != bestMachine) {
+                this.move(iterator.current.job, bestMachine);
+                System.out.println("finished BRJ, moved to: " + iterator.current.job.runningMachine.ID + " <mach | time> " + iterator.current.job.completionTime);
+            } else
+                System.out.println("finished BRJ, stayed at: " + iterator.current.job.runningMachine.ID + " <mach | time> " + iterator.current.job.completionTime);
+            System.out.println();
+            System.out.println(this.toString());
+            iterator.next();
+        }
+    }
+
+    public Machine bestResponseJob(Job job) {
+        System.out.println("Started BRJ for job: " + job.getID());
+        Machine fromMachine = job.runningMachine;
+        int startIndex = fromMachine.jobList.indexOf(job);
+        Machine toMachine = job.runningMachine;
+        int bestCompTime = job.completionTime;
+        for (Machine machine : this.machines) {
+            this.move(job, machine);
+//            System.out.println(bestCompTime + " <best | cur> " + job.completionTime);
+//            System.out.println(toMachine.ID + " <bestM | curM> " + job.runningMachine.ID);
+            if (job.completionTime < bestCompTime) {
+                System.out.println("-----switched best to mach " + job.runningMachine.ID + " time " + job.completionTime);
+                bestCompTime = job.completionTime;
+                toMachine = job.runningMachine;
+            }
+        }
+        job.runningMachine.remove(job);
+        fromMachine.jobList.add(startIndex, job);
+        job.setRunningMachine(fromMachine);
+        this.setCompletionTime();
+        return toMachine;
+    }
+
+    public void move(Job job, Machine destMachineID) {
+        int currMachineID = job.runningMachine.getID();
+        machines[currMachineID].remove(job);
+        ListIterator iterator = machines[currMachineID].jobList.iterator();
+        while (iterator.current != null) {
+            iterator.current.job.setCompletionTime();
+            iterator.next();
+        }
+        destMachineID.insert(job);
+//        System.out.println("moved job id: " + job.getID() + "   machines: " + currMachineID + " ---> " + job.runningMachine.ID);
+    }
+
     public void setCompletionTime() {
         for (Machine machine : machines) {
             machine.setCompletionTime();
@@ -148,5 +127,21 @@ public class Simulator {
             if(machines[i].jobList.getFirst()==null) return i;
         }
         return -1;
+    }
+
+    public void testMove(Job job, Machine destMachineID) {
+        try {
+            this.move(job, destMachineID);
+        } catch (Exception e) {
+            System.out.println("Exception: No such job/machine exists.\n");
+        }
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Machine machine : machines) {
+            s.append(machine.toString());
+        }
+        return s.toString();
     }
 }
